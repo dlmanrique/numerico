8af052b54e8c95398e5e5397b0782514f117b99a
dlmanrique <dl.manrique@uniandes.edu.co>
2022-04-17 18:54:51 -0500

    Corren todas las pruebas de n=2

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import animation
+import warnings 
+warnings.filterwarnings("ignore", category=RuntimeWarning) 
+
 """
 points = np.array([[0, 0], [0, 1],[2,0], [1, 0],[2,1], [1, 1]])
 tri = Delaunay(points)
@@ -13,30 +16,39 @@ plt.triplot(points[:,0], points[:,1], tri.simplices)
 plt.plot(points[:,0], points[:,1], 'o')
 plt.show()
 """
-def F(x):
-    x1,x2 = x
-    return np.array([3*x1-x2+2,
-                    2*x1**2-x2])
 
-"""
 def F(x):
     x1,x2=x
     return np.array([x1**(2)+x2**(2)-10,
                     x1-3*x2+10])
+def Fprime(x):
+    x1, x2 = x
+    Fprime = np.zeros((2, len(x)))
+    Fprime[0][0] = 2*x1
+    Fprime[0][1] =  2*x2
+    Fprime[1][0] =  1
+    Fprime[1][1] = -3
+    return Fprime
+
+"""
+def F(x):
+    x1,x2 = x
+    return np.array([3*x1-x2+2,
+                    2*x1**2-x2])
+def Fprime(x):
+    x1, x2 = x
+    Fprime = np.zeros((2, len(x)))
+    Fprime[0][0] = 3
+    Fprime[0][1] =  -1
+    Fprime[1][0] =  4*x1
+    Fprime[1][1] = -1
+    return Fprime
 """
 """
 def F(x):
     x1, x2 = x
     return np.array([1/2 * x2**(1/3) / x1**(1/2) - 1/2,
                      1/3 * x1**(1/2) / x2**(2/3) - 1/3])
-"""
-#Primer intento de definir  H(x)
-def H_N(x, lambd, x0):
-    return F(x) - (1 - lambd) * F(x0)
-
-def H_FP(x, lambd, x0):
-    return (1-lambd)*(x-x0) + lambd * F(x) 
-"""   
 def Fprime(x):
     x1, x2 = x
     Fprime = np.zeros((2, len(x)))
@@ -46,25 +58,9 @@ def Fprime(x):
     Fprime[1][1] = -2/9 * x1**( 1/2) * x2**(-5/3)
     return Fprime
 """
-"""
-def Fprime(x):
-    x1, x2 = x
-    Fprime = np.zeros((2, len(x)))
-    Fprime[0][0] = 2*x1
-    Fprime[0][1] =  2*x2
-    Fprime[1][0] =  1
-    Fprime[1][1] = -3
-    return Fprime
-"""
+def H_N(x, lambd, x0):
+    return F(x) - (1 - lambd) * F(x0)
 
-def Fprime(x):
-    x1, x2 = x
-    Fprime = np.zeros((2, len(x)))
-    Fprime[0][0] = 3
-    Fprime[0][1] =  -1
-    Fprime[1][0] =  4*x1
-    Fprime[1][1] = -1
-    return Fprime
 def derH(x):
     return Fprime(x)
 
@@ -96,7 +92,7 @@ def HomotopyMethod(x_init, lambd_step=0.05, tol=1e-5, max_iter=1000):
     result = {'lambda':[lambd], 'x':[x0]}
     
     # print initial system
-    print('Initial condition:  λ = {:.2f}, x = {} \n'.format(lambd, x0))
+    print('Initial condition:  \u03BB = {:.2f}, x = {} \n'.format(lambd, x0))
     
     # iterate lambda
     k = 0
@@ -105,14 +101,11 @@ def HomotopyMethod(x_init, lambd_step=0.05, tol=1e-5, max_iter=1000):
         lambd = min(lambd_prev + delta_lambd, 1)
         delta_lambd, xdot = predict(lambd, lambd_prev, x0, x_init)
         x1 = x0 + delta_lambd * xdot
-        
         # calculate corrector using Newton-Raphson
         i, x1 = correct(x1, lambd, x_init, tol, max_iter)
-        
-        # Newton-Raphson doesn't converge
+        # Newton-Raphson no converge
         if i == max_iter:
             delta_lambd /= 2
-        # Newton-Raphson converges, update x and lambda
         else:
             delta_lambd = lambd_step
             x0 = x1
@@ -120,25 +113,33 @@ def HomotopyMethod(x_init, lambd_step=0.05, tol=1e-5, max_iter=1000):
             k += 1
             result['x'].append(x1)
             result['lambda'].append(lambd)
-            
             # print intermediary system
-            print('Iteration: {} \t λ = {:.2f} \t x = {}'.format(k, lambd, x1))
-        
+            print('Iteration: {} \t \u03BB	 = {:.2f} \t x = {}'.format(k, lambd, x1))
         # if the Jacobian is singular, break the loop
         if np.isnan(np.sum(x1)):
-            print('Solution not found. Try other initial value of x.')
+            print('Solucion no encontrada. Ingrese otro valor inicial  x.')
             return None
-    
     # print target system
     if not np.isnan(np.sum(x1)):
-        print('Solution found: x =', x1)
+        print('Solucion: x =', x1)
     
     return result
 
-x_init = np.array([0,0])
-result = HomotopyMethod(x_init)
 
-def Newton_extendido(x):
-    pass
-
+def Newton_extendido(x,max_iterations):
+    tol = 1*10**(-5)
+    cont=0
+    while 1:
+        cont+=1
+        F_v= F(x)
+        DF_v= Fprime(x)
+        new_x = x - np.dot(np.linalg.inv(DF_v),F_v)
+        if F(new_x)[0] <= tol and F(new_x)[1] <= tol or cont >= max_iterations:
+            break
+        x = new_x
+    return new_x
+x_init = np.array([-8,20])
+w = Newton_extendido(x_init, 10000)
+print(w)
+e = HomotopyMethod(x_init)
 
