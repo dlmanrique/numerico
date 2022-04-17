1fef1feda04812374a172a0ba55b508e9f74d4a7
dlmanrique <60222741+dlmanrique@users.noreply.github.com>
2022-04-17 09:35:14 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy.spatial import Delaunay
+import matplotlib.pyplot as plt
+
+
+points = np.array([[0, 0], [0, 1],[2,0], [1, 0],[2,1], [1, 1]])
+tri = Delaunay(points)
+
+print(points[tri.simplices])
+plt.figure()
+plt.triplot(points[:,0], points[:,1], tri.simplices)
+plt.plot(points[:,0], points[:,1], 'o')
+plt.show()
